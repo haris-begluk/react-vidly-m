f3e7f1b3796921f1298d94508cb62c948722286e
haris-begluk <harisbegluk@gmail.com>
2018-09-03 11:53:08 +0200

    Exercise like component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
 
 class Movies extends Component {
   numCount = 0;
@@ -14,6 +15,16 @@ class Movies extends Component {
     this.setState({ movies });
     this.numCount = 0;
   };
+
+  handleLike = movie => {
+    //console.log("Like", movie);
+    const movies = [...this.state.movies];
+    let index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+    this.numCount = 0;
+  };
   render() {
     if (this.state.movies.length === 0)
       return <h1>There are no movies in the database!</h1>;
@@ -21,7 +32,7 @@ class Movies extends Component {
     return (
       <React.Fragment>
         <p>There are {this.state.movies.length} movies in the database!</p>
-        <table className="table col-md-6 offset-md-3">
+        <table className="table table-striped col-md-6 offset-md-3">
           <thead>
             <tr>
               <th scope="col">#</th>
@@ -29,6 +40,7 @@ class Movies extends Component {
               <th scope="col">Gender</th>
               <th scope="col">Stock</th>
               <th scope="col">Rate</th>
+              <th />
               <th />
             </tr>
           </thead>
@@ -41,6 +53,12 @@ class Movies extends Component {
                   <td>{movie.genre.name}</td>
                   <td>{movie.numberInStock}</td>
                   <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <Like
+                      liked={movie.liked}
+                      onClick={() => this.handleLike(movie)}
+                    />
+                  </td>
                   <td>
                     <button
                       className="btn btn-danger btn-sm"
