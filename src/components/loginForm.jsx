5804a21b351869cdc5836249289fdf1e4fac11d9
haris-begluk <harisbegluk@gmail.com>
2018-09-14 11:02:40 +0200

    Forms: controlled elements and handling multiple inputs

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 class LoginForm extends Component {
-  username = React.createRef(); //accessing DOM
-  //   componentDidMount() {
-  //     this.username.current.focus();
-  //   }
-
+  state = {
+    account: { username: "", password: "" }
+  };
   handleSubmit = e => {
     e.preventDefault();
     //Call the server
-    const username = this.username.current.value; //accessing DOM
+  };
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
   };
   render() {
+    const { account } = this.state;
+
     return (
       <div>
         <h1>Login Form</h1>
@@ -18,16 +22,24 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="userName">Username</label>
             <input
-              autoFocus
-              ref={this.username} //accessing DOM
-              id="userName"
+              value={account.username}
+              onChange={this.handleChange}
+              id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              onChange={this.handleChange}
+              value={account.password}
+              id="password"
+              name="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
