af9f35dea2f260511c52f74d68d7a75202fa8241
haris-begluk <harisbegluk@gmail.com>
2018-09-20 10:09:58 +0200

    HTTP: adding config.json file

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,7 +1,8 @@
 import http from "./httpService";
+import { apiUrl } from "../config.json";
 export function getMovies() {
-  return http.get("http://localhost:3900/api/movies");
+  return http.get(apiUrl + "/movies");
 }
 export function deleteMovie(movieId) {
-  return http.delete("http://localhost:3900/api/movies/" + movieId);
+  return http.delete(apiUrl + "/movies/" + movieId);
 }
