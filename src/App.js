01b4e32ff7ac8a32d2da5e30850bc57ff6ce3634
haris-begluk <harisbegluk@gmail.com>
2018-09-20 14:53:26 +0200

    AUTH: extracting protected route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NotFound from "./components/notFound";
 import Rentals from "./components/rentals";
 import Customers from "./components/customers";
 import NavBar from "./components/navBar";
+import ProtectedRoute from "./components/common/protectedRoute";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
@@ -31,16 +32,10 @@ class App extends Component {
             <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/logout" component={Logout} />
-            <Route
-              path="/movies/:id"
-              render={props => {
-                if (!user) return <Redirect to="/login" />;
-                return <MovieForm {...props} />;
-              }}
-            />
+            <ProtectedRoute path="/movies/:id" component={MovieForm} />
             <Route
               path="/movies"
-              render={props => <Movies {...props} user={this.state.user} />}
+              render={props => <Movies {...props} user={user} />}
             />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
