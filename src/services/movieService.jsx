9b680f08f052df3248ee195ac2b56b3fd2730b93
haris-begluk <harisbegluk@gmail.com>
2018-09-20 10:57:33 +0200

    HTTP: saving movie and refactoring

--- src/services/movieService.jsx
+++ src/services/movieService.jsx
@@ -1,8 +1,23 @@
 import http from "./httpService";
 import { apiUrl } from "../config.json";
+function movieUrl(id) {
+  return `${apiUrl}/movies/${id}`;
+}
+
 export function getMovies() {
   return http.get(apiUrl + "/movies");
 }
 export function deleteMovie(movieId) {
-  return http.delete(apiUrl + "/movies/" + movieId);
+  return http.delete(movieUrl(movieId));
+}
+export function getMovie(movieId) {
+  return http.get(movieUrl(movieId));
+}
+export function saveMovie(movie) {
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(movieUrl(movie._id), body);
+  }
+  http.post(apiUrl + "/movies", movie);
 }
