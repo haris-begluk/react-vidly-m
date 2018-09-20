4188780446291043e1ae75057067dcbd154ad7a9
haris-begluk <harisbegluk@gmail.com>
2018-09-20 12:02:56 +0200

    AUTH: storing JWT to local storage and handling errors

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -21,7 +21,10 @@ class LoginForm extends Form {
     try {
       const { data } = this.state;
 
-      await login(data.username, data.password);
+      const { data: jwt } = await login(data.username, data.password);
+      localStorage.setItem("token", jwt);
+      this.props.history.push("/");
+      //console.log(jwt);
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
