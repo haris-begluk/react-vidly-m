1fde33be258135851994815c51d9836342be2a21
haris-begluk <harisbegluk@gmail.com>
2018-09-20 13:50:19 +0200

    AUTH: refactoring

--- src/services/authService.jsx
+++ src/services/authService.jsx
@@ -1,7 +1,30 @@
 import http from "./httpService";
 import { apiUrl } from "../config.json";
-const apiEndpoint = apiUrl + "/auth";
+import jwtDecode from "jwt-decode";
 
-export function login(email, password) {
-  return http.post(apiEndpoint, { email, password });
+const apiEndpoint = apiUrl + "/auth";
+const tokenKey = "token";
+export async function login(email, password) {
+  const { data: jwt } = await http.post(apiEndpoint, { email, password });
+  localStorage.setItem(tokenKey, jwt);
 }
+export function loginWithJwt(jwt) {
+  localStorage.setItem(tokenKey, jwt);
+}
+export function logout() {
+  localStorage.removeItem(tokenKey);
+}
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem(tokenKey);
+    return jwtDecode(jwt);
+  } catch (ex) {
+    return null;
+  }
+}
+export default {
+  login,
+  logout,
+  getCurrentUser,
+  loginWithJwt
+};
